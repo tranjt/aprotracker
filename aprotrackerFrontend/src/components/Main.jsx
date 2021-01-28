dcc156c54e69196e11afffcaf4296a73cc1ca5dd
JTTran <tranjt80>
2021-01-28 15:57:12 +0200

    Initial app navigation overview

--- aprotrackerFrontend/src/components/Main.jsx
+++ aprotrackerFrontend/src/components/Main.jsx
@@ -2,58 +2,90 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function HomeScreen({ navigation }) {
+function ProfileScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text style={{ fontSize: 30 }}>This is the profile screen!</Text>
+    </View>
+  );
+}
+
+function HistoryScreen({ route }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 30 }}>This is the History screen!</Text>
+      <Text>{route.params?.someParam}</Text>
+    </View>
+  );
+}
+
+function WorkoutScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Workout Screen</Text>
       <Button
-        title="Go to Details"
-        onPress={() => {
-          /* 1. Navigate to the Details route with params */
-          navigation.navigate('Details', {
-            itemId: 86,
-            otherParam: 'anything you want here',
-          });
-        }}
+        onPress={() => navigation.navigate('RoutineOverview')}
+        title="Open routine overview screen (select a routine)"
       />
     </View>
   );
 }
 
-function DetailsScreen({ route, navigation }) {
-  /* 2. Get the param */
-  const { itemId, otherParam } = route.params;
+function ExerciseScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Text>itemId: {JSON.stringify(itemId)}</Text>
-      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() =>
-          navigation.push('Details', {
-            itemId: Math.floor(Math.random() * 100),
-          })
-        }
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Text>Exercise</Text>
     </View>
   );
 }
 
-const Stack = createStackNavigator();
-
-const Main = () => {
+function RoutineOverviewScreen({ navigation }) {
   return (
-    <NavigationContainer initialRouteName="Home">
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 30 }}>This is a routine overview screen!</Text>
+      <Button onPress={() => navigation.navigate('DoRoutine')} title="Do Routine" />
+    </View>
+  );
+}
+
+function DoRoutineScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Do Routine Screen!</Text>
+      <Button onPress={() => navigation.navigate('History', { someParam: 'Workout done here are the stats' } )} title="goTo History" />      
+    
+    </View>
+  );
+}
+
+
+const RootStack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Home() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="History" component={HistoryScreen} />
+      <Tab.Screen name="Workout" component={WorkoutScreen} />
+      <Tab.Screen name="Exercise" component={ExerciseScreen} />
+    </Tab.Navigator>
+  );
+}
+
+function Main() {
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator>
+        <RootStack.Screen name="Home" options={{ headerShown: false }} component={Home} />
+        <RootStack.Screen name="RoutineOverview" component={RoutineOverviewScreen} />
+        <RootStack.Screen name="DoRoutine" component={DoRoutineScreen} options={{ headerShown: false }} />
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 }
+
 
 export default Main;
