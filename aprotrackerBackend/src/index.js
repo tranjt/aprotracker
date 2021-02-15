3c14ad9e43a38637a37e0f8d6d8e3b6bc4de11d8
JTTran <tranjt80@gmail.com>
2021-02-15 20:43:37 +0200

    added mutation for routine

--- aprotrackerBackend/src/index.js
+++ aprotrackerBackend/src/index.js
@@ -1,22 +1,23 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
 
 const routines = [
   {
     name: 'Mini recommended routine',
     createdAt: "1979-01-30",
-    duration: 200,    
+    duration: 200,
     id: "rout-1"
   },
   {
     name: 'Mini recommended routine',
     createdAt: "1979-01-29",
-    duration: 200,    
+    duration: 200,
     id: "rout-2"
   },
   {
     name: 'Mini recommended routine',
     createdAt: "1979-01-29",
-    duration: 200,    
+    duration: 200,
     id: "rout-3"
   }
 ]
@@ -25,42 +26,42 @@ const exercises = [
   {
     name: 'Pull-up',
     type: 'repsOnly',
-    sets: [{ reps: 5 },{ reps: 5 },{ reps: 5 },],
+    sets: [{ reps: 5 }, { reps: 5 }, { reps: 5 },],
     createdAt: "1979-01-30",
     id: "pull-1"
   },
   {
     name: 'Squats',
     type: 'repsOnly',
-    sets: [{ reps: 5 },{ reps: 5 },{ reps: 5 },],
+    sets: [{ reps: 5 }, { reps: 5 }, { reps: 5 },],
     createdAt: "1979-01-30",
     id: "squa-1"
   },
   {
     name: 'Pull-up',
     type: 'repsOnly',
-    sets: [{ reps: 5 },{ reps: 5 },{ reps: 5 },],
+    sets: [{ reps: 5 }, { reps: 5 }, { reps: 5 },],
     createdAt: "1979-01-39",
     id: "pull-2"
   },
   {
     name: 'Squats',
     type: 'repsOnly',
-    sets: [{ reps: 5 },{ reps: 5 },{ reps: 5 },],
+    sets: [{ reps: 5 }, { reps: 5 }, { reps: 5 },],
     createdAt: "1979-01-29",
     id: "squa-2"
   },
   {
     name: 'Pull-up',
     type: 'repsOnly',
-    sets: [{ reps: 5 },{ reps: 5 },{ reps: 5 },],
+    sets: [{ reps: 5 }, { reps: 5 }, { reps: 5 },],
     createdAt: "1979-01-28",
     id: "pull-3"
-  }, 
+  },
   {
     name: 'Squats',
     type: 'repsOnly',
-    sets: [{ reps: 5 },{ reps: 5 },{ reps: 5 },],
+    sets: [{ reps: 5 }, { reps: 5 }, { reps: 5 },],
     createdAt: "1979-01-28",
     id: "squa-3"
   },
@@ -81,9 +82,23 @@ const typeDefs = gql`
     createdAt: String
     sets: [ExerciseSet]
     id: ID!
+    routineId: ID
   }
 
   type ExerciseSet {
+    kg: Int
+    reps: Int
+    time: Int
+  }
+
+  input ExerciseInput {
+    name: String!
+    description: String
+    type: String!    
+    sets: [ExerciseSetInput]   
+  }
+
+  input ExerciseSetInput {
     kg: Int
     reps: Int
     time: Int
@@ -96,18 +111,47 @@ const typeDefs = gql`
     duration: Int
     id: ID!
   }   
+  
 
   type Query {
     allRoutines: [Routine!]!
     allExercises: [Exercise!]!
   }
+
+  type Mutation {
+    addRoutine(
+      name: String!,
+      duration: Int!,
+      exercises: [ExerciseInput]
+      ): Routine
+  }
 `
+const createExercise = (args, routineId) => {
+  const newExercise = { ...args, id: uuid(), createdAt: Date(), routineId }
+  exercises.push(newExercise)
+}
+
 
 const resolvers = {
   Query: {
     allRoutines: () => routines,
     allExercises: () => exercises
+  },
+
+  Mutation: {
+    addRoutine: (root, args) => {
+      const { name, duration, exercises } = args
+      const newRoutine = { name, duration, id: uuid(), createdAt: Date() }
+      exercises.forEach(exercise => {
+        createExercise(exercise, newRoutine.id)
+      });
+
+      routines.push(newRoutine)
+      return newRoutine
+    }
   }
+
+
 }
 
 const server = new ApolloServer({
