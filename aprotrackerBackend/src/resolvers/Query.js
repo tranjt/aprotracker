111940a31253a292975c061349aae935ef8f880f
JTTran <tranjt80@gmail.com>
2021-03-04 18:42:30 +0200

    changed query allRoutines and allExercises, only return those owned by current login user

--- aprotrackerBackend/src/resolvers/Query.js
+++ aprotrackerBackend/src/resolvers/Query.js
@@ -1,13 +1,26 @@
+const { AuthenticationError } = require('apollo-server')
 const Exercise = require('../models/exercise')
 const Routine = require('../models/routine')
 
 
-const allRoutines = () => {
-  return Routine.find({}).populate('exercises').populate('user')
+const allRoutines = (root, args, context) => {
+  const currentUser = context.currentUser;
+  
+  if (!currentUser) {
+    throw new AuthenticationError('not authenticated')
+  }
+
+  return Routine.find({ user: currentUser._id }).populate('exercises').populate('user')
 }
 
-const allExercises = () => {
-  return Exercise.find({}).populate('user')
+const allExercises = (root, args, context) => {
+  const currentUser = context.currentUser;
+
+  if (!currentUser) {
+    throw new AuthenticationError('not authenticated')
+  }
+
+  return Exercise.find({user: currentUser._id}).populate('user')
 }
 
 const me = (root, args, context) => {
